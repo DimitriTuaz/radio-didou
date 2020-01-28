02ecde7adf7be524515912f7f7bf3e6429d95c66
Paul Le Couteur <paullecouteur@gmail.com>
2020-01-28 13:23:58 +0100

    remove low latency pages from static serving

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -2,28 +2,28 @@
 // Node module: @loopback/authentication
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
-import {HttpErrors} from '@loopback/rest';
-import {Credentials, UserRepository} from '../repositories/user.repository';
-import {User} from '../models/user.model';
-import {UserService} from '@loopback/authentication';
-import {UserProfile, securityId} from '@loopback/security';
-import {repository} from '@loopback/repository';
-import {PasswordHasher} from './hash.password.bcryptjs';
-import {PasswordHasherBindings} from '../keys';
-import {inject} from '@loopback/context';
+import { HttpErrors } from '@loopback/rest';
+import { Credentials, UserRepository } from '../repositories/user.repository';
+import { User } from '../models/user.model';
+import { UserService } from '@loopback/authentication';
+import { UserProfile, securityId } from '@loopback/security';
+import { repository } from '@loopback/repository';
+import { PasswordHasher } from './hash.password.bcryptjs';
+import { PasswordHasherBindings } from '../keys';
+import { inject } from '@loopback/context';
 
-export class MyUserService implements UserService<User, Credentials> {
+export class MainUserService implements UserService<User, Credentials> {
   constructor(
     @repository(UserRepository) public userRepository: UserRepository,
     @inject(PasswordHasherBindings.PASSWORD_HASHER)
     public passwordHasher: PasswordHasher,
-  ) {}
+  ) { }
 
   async verifyCredentials(credentials: Credentials): Promise<User> {
     const invalidCredentialsError = 'Invalid email or password.';
 
     const foundUser = await this.userRepository.findOne({
-      where: {email: credentials.email},
+      where: { email: credentials.email },
     });
     if (!foundUser) {
       throw new HttpErrors.Unauthorized(invalidCredentialsError);
@@ -56,6 +56,6 @@ export class MyUserService implements UserService<User, Credentials> {
       userName = user.firstName
         ? `${userName} ${user.lastName}`
         : `${user.lastName}`;
-    return {[securityId]: user.id, name: userName, id: user.id};
+    return { [securityId]: user.id, name: userName, id: user.id };
   }
 }
