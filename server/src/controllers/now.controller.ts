dc02fe81ba938f7ab4af3f1947cc19a9b87d81e8
Paul Le Couteur <paullecouteur@gmail.com>
2020-01-28 20:10:52 +0100

    [refactor] NowService is refactored in a cleaner way

--- server/src/controllers/now.controller.ts
+++ server/src/controllers/now.controller.ts
@@ -1,37 +1,30 @@
 import { get, param, getModelSchemaRef, getFilterSchemaFor } from '@loopback/rest';
-import { inject, Binding, BindingScope, Getter, bind } from '@loopback/core';
+import { inject, BindingScope, bind } from '@loopback/core';
+import { Filter, repository } from '@loopback/repository';
 
 import { RadiodBindings, RadiodKeys } from '../keys';
 
-import { NowService } from '../services/now.service';
-import { NowDeezer } from '../now/now.deezer';
-import { NowSpotify } from '../now/now.spotify';
-import { NowNone } from '../now/now.none';
-
+import { NowDeezer, NowSpotify } from '../now';
 import { NowEnum } from '@common/now/now.common';
-import { Filter, repository } from '@loopback/repository';
-
-import { NowCredentialsRepository } from '../repositories';
-import { PersistentKeyService } from '../services';
 import { NowCredentials } from '../models';
+import { NowCredentialsRepository } from '../repositories';
+import { PersistentKeyService, NowService } from '../services';
 
 import request = require('superagent');
 
 @bind({ scope: BindingScope.SINGLETON })
 export class NowController {
   constructor(
-    @inject(RadiodBindings.API_KEY) private apiKey: any,
-    @inject(RadiodBindings.GLOBAL_CONFIG) private config: any,
-    @inject(RadiodBindings.PERSISTENT_KEY_SERVICE) private persistentKey: PersistentKeyService,
+    @inject(RadiodBindings.API_KEY) private api_key: any,
+    @inject(RadiodBindings.GLOBAL_CONFIG) private global_config: any,
+    @inject(RadiodBindings.PERSISTENT_KEY_SERVICE) private params: PersistentKeyService,
     @repository(NowCredentialsRepository) public credentialRepository: NowCredentialsRepository,
-    @inject.getter(RadiodBindings.NOW_SERVICE) private serviceGetter: Getter<NowService>,
-    @inject.binding(RadiodBindings.NOW_SERVICE) private serviceBinding: Binding<NowService>
+    @inject(RadiodBindings.NOW_SERVICE) private nowService: NowService,
   ) { }
 
   @get('/now/get')
   async getNow() {
-    const service = await this.serviceGetter();
-    return service.value();
+    return this.nowService.value();
   }
 
   @get('/now/set/{credentialId}')
@@ -40,22 +33,8 @@ export class NowController {
   ) {
     try {
       let credential: NowCredentials = await this.credentialRepository.findById(credentialId);
-      await this.persistentKey.set(RadiodKeys.DEFAULT_CREDENTIAL, credential.getId());
-      let service = await this.serviceGetter();
-      let value = service.value();
-      service.stop();
-      switch (credential.type) {
-        case NowEnum.Spotify:
-          this.serviceBinding.toClass(NowSpotify).inScope(BindingScope.SINGLETON);
-          break;
-        case NowEnum.Deezer:
-          this.serviceBinding.toClass(NowDeezer).inScope(BindingScope.SINGLETON);
-          break;
-        default:
-          this.serviceBinding.toClass(NowNone).inScope(BindingScope.SINGLETON);
-      }
-      service = await this.serviceGetter();
-      service.start(value, credential.token);
+      await this.params.set(RadiodKeys.DEFAULT_CREDENTIAL, credential.getId());
+      this.nowService.setFetcher(credential);
     } catch (e) {
       console.log(e);
     }
@@ -116,7 +95,7 @@ export class NowController {
   }
 
   private async obtainSpotifyToken(code: string): Promise<any> {
-    const authorization = Buffer.from(this.apiKey.spotify.client_id + ':' + this.apiKey.spotify.secret)
+    const authorization = Buffer.from(this.api_key.spotify.client_id + ':' + this.api_key.spotify.secret)
       .toString('base64');
     const response = await request
       .post(NowSpotify.token_url)
@@ -126,7 +105,7 @@ export class NowController {
       .send({
         grant_type: 'authorization_code',
         code: code,
-        redirect_uri: this.config.loopback + 'now/1/callback'
+        redirect_uri: this.global_config.loopback + 'now/1/callback'
       });
     return {
       refresh_token: response.body.refresh_token,
@@ -152,8 +131,8 @@ export class NowController {
   private async obtainDeezerToken(code: string) {
     const response = await request
       .get(NowDeezer.auth_url)
-      .query({ app_id: this.apiKey.deezer.app_id })
-      .query({ secret: this.apiKey.deezer.secret })
+      .query({ app_id: this.api_key.deezer.app_id })
+      .query({ secret: this.api_key.deezer.secret })
       .query({ code: code })
       .query({ output: "json" });
     console.log(response.body)
---
+++ server/src/now/index.ts
@@ -0,0 +1,4 @@
+export * from './now.fetcher';
+export * from './now.none';
+export * from './now.deezer';
+export * from './now.spotify';
