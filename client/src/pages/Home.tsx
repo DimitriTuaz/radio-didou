48eeba70f03e1d70c830ef191b1e4888330e1113
Dimitri Tuaz <dimitri.tuaz@gmail.com>
2020-02-01 14:51:58 +0100

    client: add dummy sidebar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import icon_play from '../images/icon_play.png'
 import icon_sound from '../images/icon_sound.png'
 import icon_sound_low from '../images/icon_sound_low.png'
 import icon_mute from '../images/icon_mute.png'
-import { Dimmer, Loader } from 'semantic-ui-react'
+import { Dimmer, Loader, Menu, Segment, Sidebar, Icon } from 'semantic-ui-react'
 import superagent from 'superagent'
 
 import * as config from '../../../config.json';
@@ -23,6 +23,7 @@ interface IState {
   volume: number;
   playing: boolean;
   loading: boolean;
+  sidebarVisible: boolean;
   trackCover?: string;
   trackTitle?: string;
   trackArtists?: string;
@@ -46,6 +47,7 @@ class Home extends React.Component<IProps, IState> {
       volume: 1,
       playing: false,
       loading: false,
+      sidebarVisible: false,
       trackCover: undefined,
       trackTitle: undefined,
       trackArtists: undefined,
@@ -58,6 +60,8 @@ class Home extends React.Component<IProps, IState> {
     this.onPlay = this.onPlay.bind(this);
     this.onMute = this.onMute.bind(this);
     this.onChangeVolume = this.onChangeVolume.bind(this);
+    this.onClickUser = this.onClickUser.bind(this);
+    this.onClickJingles = this.onClickJingles.bind(this);
   }
 
   componentDidMount() {
@@ -151,53 +155,108 @@ class Home extends React.Component<IProps, IState> {
 
   onChangeVolume(e: React.FormEvent<HTMLInputElement>): void {
     this.audio.volume = parseFloat(e.currentTarget.value);
-    this.setState({ volume: parseFloat(e.currentTarget.value)});
+    this.setState({ volume: parseFloat(e.currentTarget.value) });
+  }
+
+  onClickUser(): void {
+
+  }
+
+  onClickJingles(): void {
+    window.open('https://www.radio-didou.com/jingles', '_blank')
+  }
+
+  onClickSidebar(show: boolean): void {
+    this.setState({ sidebarVisible: show});
   }
 
   render() {
     const isMobile = window.innerWidth <= 1000;
     return (
-      <div className='main-container'>
-        <Dimmer active={this.state.playing && this.state.loading}>
-          <Loader className='unselectable'>Chargement...</Loader>
-        </Dimmer>
-        <div className={'title-container' + (isMobile ? '-mobile' : '') + ' unselectable'}>
-          <p className={'title' + (isMobile ? '-mobile' : '')}>Radio Didou</p>
-        </div>
-        <div className={'player-container' + (isMobile ? '-mobile' : '')}>
-          <button className={'icon-sound' + (isMobile ? '-mobile' : '')} onClick={this.onPlay}>
-            <img src={icon_play} alt=''></img>
-          </button>
-          <button className={'icon-sound' + (isMobile ? '-mobile' : '')} onClick={this.onMute}>
-            <img src={this.state.mute ? icon_mute : (this.state.volume > 0.5 ? icon_sound : icon_sound_low)} alt=''></img>
-          </button>
-          <div className={'volume-slider-wrapper' + (isMobile ? '-mobile' : '')}>
-            <input
-              min={0}
-              max={1}
-              onChange={this.onChangeVolume}
-              step='any'
-              type='range'
-              value={this.state.volume}
-            />
-          </div>
-        </div>
+      <div style={{ height: '100vh', display: 'flex', flexFlow: 'column nowrap' }}>
+        <div style={{ flex: 1 }}>
+          <Sidebar.Pushable as={Segment}>
+            <Sidebar
+              as={Menu}
+              animation='overlay'
+              icon='labeled'
+              direction='right'
+              inverted
+              vertical
+              visible={this.state.sidebarVisible}
+              width='thin'
+              color='blue'
+            >
+              <Menu.Item as='a' onClick={this.onClickSidebar.bind(this, false)}>
+                <Icon name='angle right' />                
+              </Menu.Item>
+              <Menu.Item as='a' onClick={this.onClickUser}>
+                <Icon name='user' />                
+              </Menu.Item>
+              <Menu.Item as='a' onClick={this.onClickJingles}>
+                <Icon name='announcement' />
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher>
+              <Segment basic>
+                <Dimmer active={this.state.playing && this.state.loading}>
+                  <Loader className='unselectable'>Chargement...</Loader>
+                </Dimmer>
+                <div className='main-container'>
+                  <div className='header-container'>
+                    <div className={'title-container' + (isMobile ? '-mobile' : '') + ' unselectable'}>
+                      <p className={'title' + (isMobile ? '-mobile' : '')}>Radio Didou</p>
+                    </div>
+                    <div className={'settings-container'} hidden={isMobile}>
+                      <Icon 
+                        name='settings' 
+                        color='teal' // teal is the new white
+                        size='large' 
+                        onClick={this.onClickSidebar.bind(this, true)}>
+                      </Icon>
+                    </div>
+                  </div>
+                  <div className={'player-container' + (isMobile ? '-mobile' : '')}>
+                    <button className={'icon-sound' + (isMobile ? '-mobile' : '')} onClick={this.onPlay}>
+                      <img src={icon_play} alt=''></img>
+                    </button>
+                    <button className={'icon-sound' + (isMobile ? '-mobile' : '')} onClick={this.onMute}>
+                      <img src={this.state.mute ? icon_mute : (this.state.volume > 0.5 ? icon_sound : icon_sound_low)} alt=''></img>
+                    </button>
+                    <div className={'volume-slider-wrapper' + (isMobile ? '-mobile' : '')}>
+                      <input
+                        min={0}
+                        max={1}
+                        onChange={this.onChangeVolume}
+                        step='any'
+                        type='range'
+                        value={this.state.volume}
+                      />
+                    </div>
+                  </div>
 
-        <div className={'track-container' + (isMobile ? '-mobile' : '')} onClick={() => { window.open(this.trackUrl, '_blank') }}>
-          <div className='track-cover-container' >
-            <img className={'track-cover' + (isMobile ? '-mobile' : '')} src={this.state.trackCover} alt=''></img>
-          </div>
-          <div className={'track-infos-container' + (isMobile ? '-mobile' : '')}>
-            <p className={'track-title' + (isMobile ? '-mobile' : '')}>{this.state.trackTitle}</p>
-            <p className={'track-artists' + (isMobile ? '-mobile' : '')}>{this.state.trackArtists}</p>
-            <p className={'track-album' + (isMobile ? '-mobile' : '')}>{this.state.trackAlbum}</p>
-          </div>
-        </div>
+                  <div className={'track-container'}>
+                    <div className={'track-clickable' + (isMobile ? '-mobile' : '')} onClick={() => { window.open(this.trackUrl, '_blank') }}>
+                      <div className='track-cover-container' >
+                        <img className={'track-cover' + (isMobile ? '-mobile' : '')} src={this.state.trackCover} alt=''></img>
+                      </div>
+                      <div className={'track-infos-container' + (isMobile ? '-mobile' : '')}>
+                        <p className={'track-title' + (isMobile ? '-mobile' : '')}>{this.state.trackTitle}</p>
+                        <p className={'track-artists' + (isMobile ? '-mobile' : '')}>{this.state.trackArtists}</p>
+                        <p className={'track-album' + (isMobile ? '-mobile' : '')}>{this.state.trackAlbum}</p>
+                      </div>
+                    </div>
+                  </div>
 
-        <div className='current-listeners-container unselectable'>
-          <p className={'current-listeners' + (isMobile ? '-mobile' : '')}>
-            {this.state.auditorCount === undefined ? '' : this.state.auditorCount + ' auditeur' + (this.state.auditorCount > 1 ? 's' : '') + ' actuellement'}
-          </p>
+                  <div className='current-listeners-container unselectable'>
+                    <p className={'current-listeners' + (isMobile ? '-mobile' : '')}>
+                      {this.state.auditorCount === undefined ? '' : this.state.auditorCount + ' auditeur' + (this.state.auditorCount > 1 ? 's' : '') + ' actuellement'}
+                    </p>
+                  </div>
+                </div>
+              </Segment>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
         </div>
       </div>
     );
