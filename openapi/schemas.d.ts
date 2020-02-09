a69909eda63b25c3b82413d903bce038da57a2a8
Paul Le Couteur <paullecouteur@gmail.com>
2020-02-09 20:44:00 +0100

    move files

--- openapi/schemas.d.ts
+++ openapi/schemas.d.ts
@@ -1,35 +1,35 @@
 /** Loopback 4 - OpenApi Schemas **/
 
 export interface NowObject {
-  listeners: number
-  song: string
-  artists: array
-  album?: string
-  release_date?: string
-  cover?: string
-  url?: string
+listeners: number
+song: string
+artists: string[]
+album?: string
+release_date?: string
+cover?: string
+url?: string
 }
 
 export interface NowCredentials {
-  id?: string
-  userId?: string
-  name: string
-  type: number
-  token: string
+id?: string
+userId?: string
+name: string
+type: number
+token: string
 }
 
 export interface User {
-  id?: string
-  email: string
-  firstName?: string
-  lastName?: string
+id?: string
+email: string
+firstName?: string
+lastName?: string
 }
 
 export interface NewUser {
-  id?: string
-  email: string
-  firstName?: string
-  lastName?: string
-  password: string
+id?: string
+email: string
+firstName?: string
+lastName?: string
+password: string
 }
 
