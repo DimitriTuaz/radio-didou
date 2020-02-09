afad8180cf31812dbfb9b844339c1b167803b00b
Paul Le Couteur <paullecouteur@gmail.com>
2020-02-10 00:31:09 +0100

    craco adapted to include openapi generated code

--- client/craco.config.js
+++ client/craco.config.js
@@ -3,26 +3,20 @@ const path = require("path");
 module.exports = {
   plugins: [
     { plugin: require('@semantic-ui-react/craco-less') },
-    { plugin: require('@dvhb/craco-extend-scope'), options: { path: './../' } },
-    { plugin: require('@dvhb/craco-extend-scope'), options: { path: './../openapi' } }
+    { plugin: require('@dvhb/craco-extend-scope'), options: { path: './../' } }
   ],
   babel: {
     plugins: [
       ["@babel/plugin-proposal-decorators", { legacy: true }],
       ["@babel/plugin-proposal-class-properties", { loose: true }]
     ]
+
   },
   webpack: {
     alias: {
-      '../../theme.config': require('path').join(
-        __dirname,
-        '/src/semantic-ui-less/theme.config',
-      ),
-    },
-    resolve: {
-      alias: {
-        "@openapi": path.resolve(__dirname, "../openapi/"),
-      }
+      '../../theme.config': require('path').join(__dirname, '/src/semantic-ui-less/theme.config'),
+      '@openapi': path.resolve(__dirname, '../openapi/dist/'),
     }
   },
 }
+
