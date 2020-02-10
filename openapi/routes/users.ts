3807650dd6fbc890120b1999e85b58e257e98639
Paul Le Couteur <paullecouteur@gmail.com>
2020-02-10 13:03:15 +0100

    [generation] contentType done

--- openapi/routes/users.ts
+++ openapi/routes/users.ts
@@ -1,14 +1,38 @@
 /** Loopback 4 - OpenApi Route: users **/
 
 import request from 'superagent';
-import { OpenAPI } from '..'
-import * as schema from '../schemas'
+import { OpenAPI } from '..';
+import * as schema from '../schemas';
 
 export namespace UserController {
 
-    export function currentUser(): Promise<schema.User> {
-        return new Promise<schema.User>(async (resolve, reject) => {
-                        try {
+    export function login(): Promise<any> {
+        return new Promise<any>(async (resolve, reject) => {
+            try {
+                const response = await request
+                    .post(OpenAPI.URL + '/users/login');
+                resolve(response.body);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    export function logout(): Promise<any> {
+        return new Promise<any>(async (resolve, reject) => {
+            try {
+                const response = await request
+                    .post(OpenAPI.URL + '/users/logout');
+                resolve(response.body);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    export function currentUser(): Promise<object> {
+        return new Promise<object>(async (resolve, reject) => {
+            try {
                 const response = await request
                     .get(OpenAPI.URL + '/users/me');
                 resolve(response.body);
@@ -20,7 +44,7 @@ export namespace UserController {
 
     export function register(): Promise<schema.User> {
         return new Promise<schema.User>(async (resolve, reject) => {
-                        try {
+            try {
                 const response = await request
                     .post(OpenAPI.URL + '/users/register');
                 resolve(response.body);
@@ -32,7 +56,7 @@ export namespace UserController {
 
     export function findById(): Promise<schema.User> {
         return new Promise<schema.User>(async (resolve, reject) => {
-                        try {
+            try {
                 const response = await request
                     .get(OpenAPI.URL + '/users/:userId');
                 resolve(response.body);
