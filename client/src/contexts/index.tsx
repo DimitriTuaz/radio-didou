df6b5e1175867892248da2e25f2071e3b57bf4a7
Paul Le Couteur <paullecouteur@gmail.com>
2020-02-12 01:41:35 +0100

    can like song from ui!

--- client/src/contexts/index.tsx
+++ client/src/contexts/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { MainStore} from '../stores/MainStore'
 import { UserStore } from '../stores/UserStore'
+import { SongStore } from '../stores/SongStore'
 
 export const storesContext = React.createContext({
   mainStore: new MainStore(),
-  userStore: new UserStore()
+  userStore: new UserStore(),
+  songStore: new SongStore()
 })
