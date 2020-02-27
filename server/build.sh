6cbdaedb2ed64236d0fff3ccf6e35b6a079f8ff6
Paul Le Couteur <paullecouteur@gmail.com>
2020-02-27 15:01:54 +0100

    add clean dist in the server build

--- server/build.sh
+++ server/build.sh
@@ -2,10 +2,12 @@
 if [[ (-z "$1") || (! $1 == "openapi") ]];
 then
 echo "## BUILD SERVER ##"
+rm -rf dist/
 npm run build
 fi
 echo "## GENERATE OPENAPI CLIENT ##"
 node ./dist/generate-api
 echo "## BUILD OPENAPI CLIENT ##"
 cd ../openapi
+rm -rf dist/
 npm run build
