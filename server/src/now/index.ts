27316c79a53a35ad3395136ec03c92596e636ae4
Paul Le Couteur <paullecouteur@gmail.com>
2020-04-08 18:41:31 +0200

    [server] add keys

--- server/src/now/index.ts
+++ server/src/now/index.ts
@@ -1,17 +1,6 @@
-import { BindingKey } from '@loopback/core';
-import { NowFetcher, NowObject } from './now.fetcher';
-import { NowService, NowState } from './now.service';
-import { NowComponent } from './now.component';
-
-export namespace NowBindings {
-  export const NOW_SERVICE = BindingKey.create<NowService>('now.service');
-  export const CURRENT_NOW = BindingKey.create<NowObject>('now.current-now')
-  export const NOW_FETCHER = BindingKey.create<NowFetcher>('now.fetcher');
-  export const NOW_TOKEN = BindingKey.create<string>('now.token');
-  export const NOW_STATE = BindingKey.create<NowState>('now.state');
-  export const COMPONENT = BindingKey.create<NowComponent>('components.NowComponent');
-}
-
+// BINDINGS
+export * from './now.keys';
+// UTILS
 export * from './now.fetcher';
 export * from './now.none';
 export * from './now.deezer';
