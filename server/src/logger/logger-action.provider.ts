0e9e3ecfa633f1908eb5ac16552d3405293a80ee
Paul Le Couteur <paullecouteur@gmail.com>
2020-04-22 02:17:51 +0200

    improve logging

--- server/src/logger/logger-action.provider.ts
+++ server/src/logger/logger-action.provider.ts
@@ -13,30 +13,25 @@ export class LoggerActionProvider implements Provider<LogFn> {
     @inject.getter(LoggingBindings.METADATA) private metadata: Getter<LoggerEnhancedMetadata | undefined>
   ) { }
 
-  value(): LogFn { return (req, args, start) => this.action(req, args, start); }
+  value(): LogFn {
+    return (req, args, start) => this.action(req, args, start);
+  }
 
   private async action(req: Request, args: OperationArgs, start: bigint) {
 
-    let timems: bigint = (process.hrtime.bigint() - start) / BigInt(1e+6);
-    let enhancedMetadata = await this.metadata();
+    let time: bigint = (process.hrtime.bigint() - start) / BigInt(1e+6);
+    let info = await this.metadata();
     let msg: string = '';
 
-    if (enhancedMetadata == undefined) return;
+    if (info == undefined) return;
 
-    const level = enhancedMetadata.metadata ? enhancedMetadata.metadata.level : LOGGER_LEVEL.DEBUG;
-    const showArgs = enhancedMetadata.metadata ? enhancedMetadata.metadata.showArgs : true;
-
-    let string = args !== undefined ? args : [];
-    if (showArgs)
-      string = string.map(value => JSON.stringify(value));
+    const level = info.metadata ? info.metadata.level : LOGGER_LEVEL.DEBUG;
 
     msg += `ip=${req.ip} - `;
-    if (enhancedMetadata.currentUser !== undefined)
-      msg += `userId=${enhancedMetadata.currentUser[securityId]} - `
-    msg += `time=${timems}ms - `;
-    msg += `method=${enhancedMetadata.className}.${enhancedMetadata.methodName}`;
-    if (string.length > 0)
-      msg += `\n| args > ${string.join(', ')}`;
+    if (info.currentUser !== undefined)
+      msg += `userId=${info.currentUser[securityId]} - `
+    msg += `time=${time}ms - `;
+    msg += `method=${info.className}.${info.methodName}`;
 
     this.logger.log(level, msg);
   }
